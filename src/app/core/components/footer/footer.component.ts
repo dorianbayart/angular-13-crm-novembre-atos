ea6272cc1f14d764833a8a2e6373a728e13046d8
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2021-11-22 17:31:29 +0100

    7.1 - Correction TP2

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  public prenomAlea : string = 'Choix aléa';
   constructor() { }
 
   ngOnInit(): void {
+    const tabPrenom = ['Amal', 'Alexandre', 'Zied', 'Erwan', 'Thomas', 'Paul', 'Grégory', 'Dorian'];
+    const nombreAlea = Math.floor(Math.random() * tabPrenom.length );
+
+    this.prenomAlea = tabPrenom[nombreAlea];
+    console.log(this.prenomAlea);
   }
 
 }
