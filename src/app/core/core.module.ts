87f895d2b4a50b3d5b63a963420677c5d92e3433
Dorian Bayart <dorian.bayart@gmail.com>
2021-11-22 14:30:10 +0100

    2 - import CoreModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { LoginModule } from '../login/login.module';
+import { UiModule } from '../ui/ui.module';
+import { TemplatesModule } from '../templates/templates.module';
+import { IconsModule } from '../icons/icons.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  exports: [LoginModule, UiModule, TemplatesModule, IconsModule],
 })
-export class CoreModule { }
+export class CoreModule {}
