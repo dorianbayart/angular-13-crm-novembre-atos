d23bc705fef24f44db636f8bc1ef9ae215d7d826
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2021-11-23 10:07:53 +0100

    8 - ngclass + eventBinding

--- src/app/ui/components/ui/ui.component.ts
+++ src/app/ui/components/ui/ui.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ui.component.scss']
 })
 export class UiComponent implements OnInit {
-
+  public open : boolean  = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public toggle(): void {
+    this.open = !this.open;
+    console.log('La valeur de la prop est : ', this.open);
+    
+  }
 }
