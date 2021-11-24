aa562539641ffe433f1c29dfe516925c76ad43f3
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2021-11-24 14:53:16 +0100

    22 - input data binding + lifecycle hooks

--- src/app/orders/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/page-list-orders/page-list-orders.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnDestroy, OnInit } from '@angular/core';
 import { StateOrder } from 'src/app/core/enums/state-order';
 import { Order } from 'src/app/core/models/order';
 import { OrderService } from '../services/order.service';
@@ -8,9 +8,12 @@ import { OrderService } from '../services/order.service';
   templateUrl: './page-list-orders.component.html',
   styleUrls: ['./page-list-orders.component.scss']
 })
-export class PageListOrdersComponent implements OnInit {
+export class PageListOrdersComponent implements OnInit, OnChanges, OnDestroy {
+  public titrePage!: {name: string};
 
   constructor(private orderService: OrderService) {
+    console.log('Page List ---New Instance');
+    this.titrePage = {name: 'List Order'};
     this.orderService.collection$.subscribe( 
       {
         next: (data) => console.log(data),
@@ -21,7 +24,20 @@ export class PageListOrdersComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    console.log('Page List ---OnInit');
+    // Liaison des propriétés de la class avec le template
+  }
+
+  public changeTitle(): void {
+    this.titrePage = {name: "Encore un super titre !!"};
+  }
+
+  ngOnChanges(): void {
+    console.log('Page List ---OnChanges');
+  }
   
+  ngOnDestroy(): void {
+    console.log('Page List ---OnDestroy');
   }
 
 }
--- src/app/templates/components/template-full-width/template-full-width.component.ts
+++ src/app/templates/components/template-full-width/template-full-width.component.ts
@@ -1,17 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-template-full-width',
   templateUrl: './template-full-width.component.html',
   styleUrls: ['./template-full-width.component.scss']
 })
-export class TemplateFullWidthComponent implements OnInit {
-  public title: string;
+export class TemplateFullWidthComponent implements OnInit, OnChanges, OnDestroy, DoCheck {
+  @Input('titreAttribut') public title!: {name: string};
   constructor() {
-    this.title = 'Le titre est ici';
-   }
-
-  ngOnInit(): void {
+    console.log('Template Full W ---New Instance');
   }
 
+  ngOnInit(): void {
+    console.log('Template Full W ---OnInit');
+    console.log('Template Full W ---OnInit', this.title);
+  }
+
+  ngOnChanges(): void {
+    console.log('Template Full W ---OnChanges');
+    console.log('Template Full W ---OnChanges', this.title);
+  }
+
+  ngDoCheck(): void {
+    console.log('Template Full W ---OnDoCheck');
+    console.log('Template Full W ---OnDoCheck', this.title);
+  }
+
+  ngOnDestroy(): void {
+    console.log('Template Full W ---OnDestroy');
+    console.log('Template Full W ---OnDestroy', this.title);
+  }
 }
