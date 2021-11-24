93435f71d53d31ed2c9bfb806b4bca0c1542b21f
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2021-11-24 17:23:34 +0100

    25 - projection tableau dynamique

--- src/app/shared/components/table-light/table-light.component.ts
+++ src/app/shared/components/table-light/table-light.component.ts
@@ -8,25 +8,20 @@ import { Order } from 'src/app/core/models/order';
 })
 export class TableLightComponent implements OnInit, OnChanges {
   @Input() public headers!: string[];
-  @Input() public data!: Order[];
 
   constructor() {
     console.log('new Instance');
     console.log(this.headers);
-    console.log(this.data);
   }
 
   ngOnInit(): void {
     console.log('onInit');
     console.log(this.headers);
-    console.log(this.data);
   }
 
   ngOnChanges(): void {
     console.log('onChange');
     console.log(this.headers);
-    console.log(this.data);
-    
   }
 
 
