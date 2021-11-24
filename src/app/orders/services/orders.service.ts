53c6789dd61ddd2605bb98623910ad6354a45d7a
Dorian Bayart <dorian.bayart@gmail.com>
2021-11-24 12:19:33 +0100

    21 - templates de page

--- src/app/orders/services/orders.service.ts
+++ src/app/orders/services/orders.service.ts
@@ -11,29 +11,7 @@ export class OrdersService {
   public collection$!: Observable<Order[]>;
   private url = environment.urlApi;
 
-  // ----
-  private obs = new Observable((fluxData) => fluxData.next(Math.random()));
-  private sub = new Subject();
-  private bSub = new BehaviorSubject(123);
-  // ----
-
   constructor(private http: HttpClient) {
     this.collection$ = this.http.get<Order[]>(`${this.url}/orders`);
-
-    // ----
-    this.obs.subscribe((data) => console.log('Observer 1: ', data));
-    this.obs.subscribe((data) => console.log('Observer 2: ', data));
-
-    this.sub.subscribe((data) => console.log('Subject Observer 1: ', data));
-    this.sub.subscribe((data) => console.log('Subject Observer 2: ', data));
-    this.sub.next('Envoi de la 1ère Newsletter');
-
-    this.bSub.subscribe((data) => console.log('BehaviorSubject Observer 1: ', data));
-    this.bSub.subscribe((data) => console.log('BehaviorSubject Observer 2: ', data));
-    this.bSub.next(456);
-    this.bSub.subscribe((data) => console.log('BehaviorSubject Observer 3: ', data));
-    this.bSub.next(789);
-
-    // ----
   }
 }
