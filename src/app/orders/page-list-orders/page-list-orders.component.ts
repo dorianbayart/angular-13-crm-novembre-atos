176e887a946bd41f52a975ef601587391719dca6
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2021-11-24 09:50:15 +0100

    18 - enum interface model + commentaire demo

--- src/app/orders/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/page-list-orders/page-list-orders.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { StateOrder } from 'src/app/core/enums/state-order';
+import { Order } from 'src/app/core/models/order';
 
 @Component({
   selector: 'app-page-list-orders',
@@ -10,6 +12,20 @@ export class PageListOrdersComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const test1 = new Order();
+    console.log(test1);
+
+    let test2 = new Order({
+      tjmHt : 1200,
+      nbJours : 1,
+      tva : 20,
+      state : StateOrder.OPTION,
+      typePresta: 'Formation',
+      client: 'Cap',
+      comment: 'idjeijde',
+      id: 0
+    })
+    console.log(test2);
   }
 
 }
