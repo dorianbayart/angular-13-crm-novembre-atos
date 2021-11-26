03cf5c87b7e120540a49765d6b76fd24db618dca
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2021-11-26 12:54:01 +0100

    final - TP Client

--- src/app/clients/clients-routing.module.ts
+++ src/app/clients/clients-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PageAddClientComponent } from './page-add-client/page-add-client.component';
-import { PageEditClientComponent } from './page-edit-client/page-edit-client.component';
-import { PageListClientsComponent } from './page-list-clients/page-list-clients.component';
+import { PageAddClientComponent } from './page/page-add-client/page-add-client.component';
+import { PageEditClientComponent } from './page/page-edit-client/page-edit-client.component';
+import { PageListClientsComponent } from './page/page-list-clients/page-list-clients.component';
 
 const routes: Routes = [
-  { path: '', component: PageListClientsComponent},
-  { path: 'add', component: PageAddClientComponent},
-  { path: 'edit', component: PageEditClientComponent}
+  { path: '', component: PageListClientsComponent },
+  { path: 'add', component: PageAddClientComponent },
+  { path: 'edit/:clientId', component: PageEditClientComponent }
 ];
 
 @NgModule({
--- src/app/shared/pipes/total.pipe.ts
+++ src/app/shared/pipes/total.pipe.ts
@@ -1,18 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Order } from 'src/app/core/models/order';
 
 @Pipe({
   name: 'total'
 })
 export class TotalPipe implements PipeTransform {
 
-  transform(order: Order, ttc?: boolean): number {
+  transform(item: any, ttc?: boolean): number {
     if(ttc) {
-      return order.totalTTC();
+      return item.totalTTC();
     }
-    return order.totalHT();
-
-    //return ttc ? order.totalTTC() : order.totalHT();
+    return item.totalHT();
   }
 
 }
